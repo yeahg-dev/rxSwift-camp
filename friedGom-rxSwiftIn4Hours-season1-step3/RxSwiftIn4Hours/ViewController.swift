3c2054b77ea7fdb93544e86bf710c03dbc10ebc8
yeahg-dev <yeahg.dev@gmail.com>
2022-05-20 13:55:06 +0900

    🔨feat: idField, psField 검증 로직, UI 처리 구현

--- friedGom-rxSwiftIn4Hours-season1-step3/RxSwiftIn4Hours/ViewController.swift
+++ friedGom-rxSwiftIn4Hours-season1-step3/RxSwiftIn4Hours/ViewController.swift
@@ -34,6 +34,29 @@ class ViewController: UIViewController {
         //          +--> button enable
         //          |
         // pw input +--> check valid --> bullet
+        
+        idField.rx.text.orEmpty
+            .map(checkEmailValid)
+            .subscribe(onNext: {result in
+                self.idValidView.isHidden = result
+            })
+            .disposed(by: disposeBag)
+        
+        pwField.rx.text.orEmpty
+            .map(checkPasswordValid)
+            .subscribe(onNext: {result in
+                self.pwValidView.isHidden = result
+            })
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(
+            idField.rx.text.orEmpty.map(checkEmailValid),
+            pwField.rx.text.orEmpty.map(checkPasswordValid),
+            resultSelector: {s1, s2 in s1 && s2})
+        .subscribe(onNext: {result in
+            self.loginButton.isEnabled = result
+        })
+        .disposed(by: disposeBag)
     }
 
     // MARK: - Logic
